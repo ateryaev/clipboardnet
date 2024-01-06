d9f93d83079891ab5298f17f324873f413eea496
Anton Teryaev <unfinder@gmail.com>
2024-01-06 18:12:46 +0200

    Database rules. Cleaning code.

--- src/components/PageWatch.js
+++ src/components/PageWatch.js
@@ -29,16 +29,9 @@ export function PageWatch({ onCancel, ...props }) {
     const prewords = clip.text.replaceAll("\n", " \n").split(/[ \t]/);
     return prewords.map((word) => {
       if (word.trim().indexOf("https://") === 0) {
-        return <><a className="group text-blue-600
-      hover:underline
-       focus-visible:bg-blue-600 focus-visible:text-white 
-      " target="_blank" href={word}>{word}
-          <svg className="w-3 text-blue-600 absolute inline
-        group-hover:mt-[-1px] group-focus-visible:mt-[-1px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" >
-            <path fillRule="evenodd" d="M8.914 6.025a.75.75 0 0 1 1.06 0 3.5 3.5 0 0 1 0 4.95l-2 2a3.5 3.5 0 0 1-5.396-4.402.75.75 0 0 1 1.251.827 2 2 0 0 0 3.085 2.514l2-2a2 2 0 0 0 0-2.828.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-            <path fillRule="evenodd" d="M7.086 9.975a.75.75 0 0 1-1.06 0 3.5 3.5 0 0 1 0-4.95l2-2a3.5 3.5 0 0 1 5.396 4.402.75.75 0 0 1-1.251-.827 2 2 0 0 0-3.085-2.514l-2 2a2 2 0 0 0 0 2.828.75.75 0 0 1 0 1.06Z" clipRule="evenodd" />
-          </svg>
-        </a> </>
+        return <><a className="group text-blue-600 hover:underline
+        focus-visible:bg-blue-600 focus-visible:text-white"
+          target="_blank" href={word}>{word}</a> </>
       }
       return <>{word} </>;
     })
--- src/components/UserContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext({ online: false, owns: {}, subs: {} });
